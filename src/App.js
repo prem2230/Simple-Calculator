8d3679fa4b30ddb48ac81d5402e1a71f71034b72
PremKumar <109847674+prem2230@users.noreply.github.com>
2023-11-22 01:26:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [result, setResult]=useState("");
+
+  const handleClick=(value)=>{
+    if (value === '='){
+      try{
+      setResult(eval(result).toString())
+      }catch(error){
+        setResult('Error')
+      }
+    }
+    else if(value==='AC'){
+      setResult('')
+
+    }else if(value ==='\u2190'){
+    setResult((prevInput) => prevInput.slice(0,-1))
+  }else{
+    setResult((prevInput)=>prevInput+value)
+  }
+
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="container">
+      <form>
+        <input type="text" value={result} />
+      </form>
+      <div className="keypad">
+        <div>
+              <button onClick = {()=> handleClick('AC')}>AC</button>
+              <button onClick = {()=> handleClick('\u2190')}>&larr;</button>
+              <button onClick = {()=> handleClick('\u0025')}>&#37;</button>
+              <button onClick = {()=> handleClick('/')} className="span">&divide;</button>
+        </div>
+        <div>
+              <button onClick = {()=> handleClick('7')}>7</button>
+              <button onClick = {()=> handleClick('8')}>8</button>
+              <button onClick = {()=> handleClick('9')}>9</button>
+              <button onClick = {()=> handleClick('*')} className="span">&times;</button>
+        </div>
+        <div>
+              <button onClick = {()=> handleClick('4')}>4</button>
+              <button onClick = {()=> handleClick('5')}>5</button>
+              <button onClick = {()=> handleClick('6')}>6</button>
+              <button onClick = {()=> handleClick('-')} className="span">&ndash;</button>
+        </div>
+        <div>
+              <button onClick = {()=> handleClick('1')}>1</button>
+              <button onClick = {()=> handleClick('2')}>2</button>
+              <button onClick = {()=> handleClick('3')}>3</button>
+              <button onClick = {()=> handleClick('+')} className="span">+</button>
+        </div>
+        <div>
+              <button onClick = {()=> handleClick('00')}>00</button>
+              <button onClick = {()=> handleClick('0')}>0</button>
+              <button onClick = {()=> handleClick('.')}>.</button>
+              <button onClick = {()=> handleClick('=')} className="span">=</button>
+        </div>
+         
+      </div>
+
     </div>
+    
+    
   );
 }
 
